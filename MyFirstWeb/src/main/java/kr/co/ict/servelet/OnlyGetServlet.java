1c2ca9c1273eaa84bcd3d586c5b20a56275d1da3
lee-ruru <vkfwl700@gmail.com>
2022-03-04 18:37:21 +0900

    boardSevelet,DAO,VO

---
+++ MyFirstWeb/src/main/java/kr/co/ict/servelet/OnlyGetServlet.java
@@ -0,0 +1,47 @@
+package kr.co.ict.servelet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OnlyGetServlet
+ */
+@WebServlet("/getServlet")
+public class OnlyGetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OnlyGetServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("/getservlet 최초접속");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("/getservlet 접속시 생성된 객체는 서버 종료로 인해 파기됩니다.");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("/getservlet get방식 접속에 성공했습니다.");
+	}
+
+}
